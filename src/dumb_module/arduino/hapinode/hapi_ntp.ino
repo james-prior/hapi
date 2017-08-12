c856b008f604e04bec43de5952cad2dd098c6ec6
James Prior <jep200404@columbus.rr.com>
2017-08-11 22:40:20 -0400

    src/dumb_module/arduino/hapinode/hapi_ntp.ino: getNtpTime(): Convert while loop to for loop.

--- src/dumb_module/arduino/hapinode/hapi_ntp.ino
+++ src/dumb_module/arduino/hapinode/hapi_ntp.ino
@@ -65,8 +65,7 @@ time_t getNtpTime(void)
     ;
   Serial.println(F("Transmit NTP Request"));
   sendNTPpacket(ntpServerIP);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
+  for (uint32_t beginWait = millis(); millis() - beginWait < 1500; ) {
     int size = udp.parsePacket();
     if (size < sizeof(packet))
       continue;
